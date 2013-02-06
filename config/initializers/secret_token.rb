4c3acf257f7c3c2914e5327fb5faa22d2737c5d4
Andrii Rudenko <kolobock@gmail.com>
2013-02-06 18:25:44 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuessNumber::Application.config.secret_token = 'dca7104b6cd8594fe5be6ee0a12773f863fc317ee4a86f8b6572fcf1333920274918c75533f110465f13d4ded9c387351344bfbc800dbb93fdaedb079cd0f24d'
